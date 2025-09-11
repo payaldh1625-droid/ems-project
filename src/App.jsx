3189cf2ee706207d9e2e96cad6409f602ab7abfe
Payal Rani <payaldh1625@gmail.com>
2025-09-12 00:30:17 +0530

    vhbjntyguh

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   
 const [user, setUser] = useState(null) 
 const [loggedInUserData, setLoggedInUserData] = useState(null)
-const authData = useContext(AuthContext)
+const [userData,setUserData] = useContext(AuthContext)
 
 useEffect(()=>{
   const loggedInUser = localStorage.getItem('loggedInUser')
@@ -29,12 +29,12 @@ if (email == 'admin@me.com' && password =='123') {
 setUser('admin')
 localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'})) 
 } 
-else if (authData) {
-  const employee = authData.employees.find((e)=>email == e.email && e.password == password)
+else if (userData) {
+  const employee = userData.find((e)=> email == e.email && e.password == password)
   if(employee){
   setUser('employee') 
   setLoggedInUserData(employee)
-  localStorage.setItem('loggedInUser',JSON.stringify({role:'employee'})) 
+  localStorage.setItem('loggedInUser',JSON.stringify({role:'employee',data:employee})) 
   }
 } 
 
